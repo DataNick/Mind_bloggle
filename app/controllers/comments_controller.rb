8f3a4b4409925673386ec5378868fbed56e0c290
DataNick <DataNick@users.noreply.github.com>
2015-07-18 16:48:34 -0400

    Added comment functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(comment_params)
+
+		redirect_to post_path(@post)
+		# if @comment.save
+		# 	redirect_to @post
+		# else
+		# 	flash.now[:danger] = "error"
+		# 	redirect_to @comment.post
+		# end
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:name, :body)
+	end
+end
